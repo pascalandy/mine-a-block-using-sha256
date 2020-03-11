cb2feacca7b6d016cee39eb92226f23b907c2ba2
Pascal Andy <pascal@firepress.org>
2020-03-11 15:45:14 -0400

    Update mining demo (squash)

--- add-on/example_custom_scripts_entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# load all your custom script from here
-#source "${local_bashlava_addon_path}/custom_myapp1.sh"
-#source "${local_bashlava_addon_path}/custom_myapp2.sh"
--- add-on/example_custom_urls.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-function firepress { open https://firepress.org/
-}
-function github { open https://github.com/firepress-org/bashlava
-}
